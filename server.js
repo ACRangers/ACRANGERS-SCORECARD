749c3e0508ba09ad5415a993f0dfbd313f7b168d
ACRangers <aragonjohn70@gmail.com>
2026-05-06 16:58:13 -0700

    Use DATABASE_URL with correct Railway hostname db.railway.internal

--- server.js
+++ server.js
@@ -10,11 +10,7 @@ app.use(cors())
 app.use(express.json())
 
 const pool = new pg.Pool({
-  host: process.env.PGHOST,
-  port: process.env.PGPORT,
-  database: process.env.PGDATABASE,
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
+  connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
 })
 
